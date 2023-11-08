0507efac108e0722bef2b5d94880f5d2877f7877
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2023-11-07 21:41:13 -0500

    Don't include imports in tests file.

--- restarts-test.scm
+++ restarts-test.scm
@@ -1,19 +1,3 @@
-(cond-expand
-  (guile
-   (import (scheme base)
-           (restarts)
-           (srfi srfi-64)))
-  (chibi
-   (import (scheme base)
-           (restarts)
-           (rename (except (chibi test) test-equal)
-                   (test test-equal))))
-  (else
-   (import (scheme base)
-           (restarts)
-           (srfi 64))))
-
-
 (test-begin "Restarts")
 
 (test-group
