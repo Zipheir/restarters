ec6b2399479236085453c95a80bcb2ef62374bde
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2023-11-09 14:14:57 -0500

    Location info for errors and assertions.

--- restarts-impl.scm
+++ restarts-impl.scm
@@ -1,8 +1,8 @@
 (define-syntax assert-type
   (syntax-rules ()
-    ((assert-type test . args)
+    ((assert-type loc test . args)
      (unless test
-       (error "type check failed" 'expr . args)))))
+       (error loc "type check failed" 'expr . args)))))
 
 (define-record-type <restarter>
   (make-raw-restarter tag description invoker)
@@ -13,10 +13,11 @@
 
 ;; Exported constructor.
 (define (make-restarter tag description invoker)
-  (assert-type (symbol? tag))
-  (assert-type (and (pair? description)
+  (assert-type 'make-restarter-tag (symbol? tag))
+  (assert-type 'make-restarter-tag
+               (and (pair? description)
                     (every string? description)))
-  (assert-type (procedure? invoker))
+  (assert-type 'make-restarter-tag (procedure? invoker))
   (make-raw-restarter tag description invoker))
 
 (define (restart restarter . args)
@@ -24,7 +25,7 @@
 
 (define ambient-restarters (make-parameter '()))
 
-(define (restarters->list restarters allow-compound?)
+(define (restarters->list loc restarters allow-compound?)
   ;; Ensure that each element of *restarters* is a
   ;; restarter with a unique tag (with respect to the
   ;; rest of the list).
@@ -32,9 +33,9 @@
     (let loop ((elts restarters) (seen-tags '()))
       (when (pair? elts)
         (let* ((r (car elts)) (tag (restarter-tag r)))
-          (assert-type (restarter? r) r)
+          (assert-type loc (restarter? r) r)
           (when (memv tag seen-tags)
-            (error "duplicate tag in restarter list" tag))
+            (error loc "duplicate tag in restarter list" tag))
           (loop (cdr elts) (cons tag seen-tags))))))
 
   (cond
@@ -47,10 +48,11 @@
            (restarters (filter restarter? subobjs)))
       restarters))
    (allow-compound?
-    (error "not a restarter, list of restarters, or a compound object"
+    (error loc
+           "not a restarter, list of restarters, or a compound object"
            restarters))
    (else
-    (error "not a restarter or a list of restarters" restarters))))
+    (error loc "not a restarter or a list of restarters" restarters))))
 
 (define (with-restarters restarters thunk)
   (parameterize ((ambient-restarters (collect-restarters restarters)))
@@ -59,11 +61,11 @@
 (define (find-restarter tag restarters)
   (let ((pred (lambda (restarter)
                 (eqv? tag (restarter-tag restarter)))))
-    (or (find pred (restarters->list restarters #t))
+    (or (find pred (restarters->list 'find-restarter restarters #t))
         (find pred (ambient-restarters)))))
 
 (define (collect-restarters restarters)
-  (let ((list (restarters->list restarters #t)))
+  (let ((list (restarters->list 'collect-restarters restarters #t)))
     (append list
             (lset-difference (lambda (r1 r2)
                                (eqv? (restarter-tag r1)
